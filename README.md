8bc2a0ce8f98f1def1e9aea31572f5ba69230d10
maoshuochen <maoshuochen@gmail.com>
2022-04-01 00:23:45 +0800

    Done!

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-
 # [Timestamp Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice)
+
+Course Link: https://chinese.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/timestamp-microservice
--- server.js
+++ server.js
@@ -2,31 +2,65 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// empty date params
+app.get("/api", function (req, res) {
+    date = new Date();
+    utc = date.toUTCString();
+    unix = date.getTime();
+    console.log({ date, utc, unix });
+    res.json({ unix, utc });
 });
 
+// timestape convert
+app.get("/api/:date", function (req, res) {
+    console.log(req.params.date);
+    let date = req.params.date;
+    let unix, utc;
 
+    if (date instanceof Date) {
+        //valid date
+        utc = date.toUTCString();
+        unix = date.getTime();
+    } else if (!isNaN(date)) {
+        //unix to date
+        unix = parseInt(date);
+        date = new Date(unix);
+        utc = date.toUTCString();
+    } else {
+        //try to convert
+        date = new Date(date);
+        utc = date.toUTCString();
+        unix = date.getTime();
+    }
+
+    console.log({ date, utc, unix });
+    if (utc == "Invalid Date") {
+        console.log("Error");
+        res.json({
+            error: "Invalid Date",
+        });
+    } else {
+        res.json({ unix, utc });
+    }
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(3000, function () {
+    console.log("Your app is listening on port " + listener.address().port);
 });
